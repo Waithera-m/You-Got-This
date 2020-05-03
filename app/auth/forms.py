5ee2ca0ea9eaa7de57176cf1e0d93377356af830
Mary Waithera <njihiamary11@gmail.com>
2020-05-03 11:38:42 +0300

    Create registration form, template to render this form and blueprint view function to render this template

---
+++ app/auth/forms.py
@@ -0,0 +1,33 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
+from wtforms import ValidationError
+
+class UserRegistrationForm(FlaskForm):
+
+    '''
+    class facilitates the creation of form input fields
+    '''
+    email = StringField('Enter your email address',validators=[Required(),Email()])
+    username = StringField('Enter your username',validators=[Required()])
+    password = PasswordField('Enter your password',validators=[Required(),EqualTo('password_confirm',message = 'Both passwords must match')])
+    password_confirm = PasswordField('Confirm your password',validators=[Required()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self,data_field):
+
+        '''
+        function checks if email is already in the database and if it is, it raises a ValidationError
+        '''
+        if User.query.filter_by(email=data_field.data).first():
+            raise ValidationError('The email already exists')
+
+    def validate_username(self,data_field):
+
+        '''
+        function checks if there is an account that uses the entered username and if there is, it raises a ValidationError
+        '''
+        if User.query.filter_by(username=data_field.data).first():
+            raise ValidationError('The username already exists')
+
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,10 +1,28 @@
-from flask import render_template
+from flask import render_template,redirect,url_for
 from . import auth
+from ..models import User
+from .forms import UserRegistrationForm
+from .. import db
 
-@auth.route('/login')
+@auth.route('/login', methods=["GET","POST"])
 def login():
 
     '''
     view function renders login.html
     '''
     return render_template('auth/login.html')
+
+@auth.route('/register',methods=["GET","POST"])
+def register ():
+
+    '''
+    view function renders registration form
+    '''
+    form = UserRegistrationForm()
+    if form.validate_on_submit():
+        User = User(email=form.email.data,username=form.username.data,password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(urls_for('.login'))
+        title="New Account"
+    return render_template('auth/register.html',registration_form=form)
