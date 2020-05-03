d3adb06cf29863fee26dcf4ba053c10d7cfa79ed
Mary Waithera <njihiamary11@gmail.com>
2020-05-03 10:11:08 +0300

    add password column to users' table; create and test functions that prevent access to password property, hash and verify hashed passwords

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
 
@@ -9,6 +10,30 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(255))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_secure = db.Column(db.String(255))
+    
+    @property
+    def password(self):
+
+        '''
+        function blocks access to password property
+        '''
+        raise AttributeError('Password attribute cannot be read')
+
+    @password.setter
+    def password(self,password):
+
+        '''
+        function generates password hash
+        '''
+        self.password_secure = generate_password_hash(password)
+        
+    def verify_password(self,password):
+
+        '''
+        function checks if entered and hashed passwords match
+        '''
+        return check_password_hash(self.password_secure,password)
 
     def __repr__(self):
         return f'User {user.username}'
