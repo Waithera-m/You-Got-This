82d53040ff8e2082b75a0ada1b746ccac937d1f0
Mary Waithera <njihiamary11@gmail.com>
2020-05-03 09:30:33 +0300

    Create user and role classes to define users' objects and roles respectively; configure manage.py to create a db shell

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from . import db
+
+class User(db.Model):
+
+    '''
+    class faciliates the creation of user objects
+    '''
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(255))
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return f'User {user.username}'
+
+class Role(db.Model):
+
+    '''
+    class facilitates the creation of role objects
+    '''
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref='role',lazy='dynamic')
+
+    def __repr__(self):
+        return f'User {self.name}'
