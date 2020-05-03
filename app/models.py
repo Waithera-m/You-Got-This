74672c3f4ec9c614cc8fa1821e9ecb2d8ed21e42
Mary Waithera <njihiamary11@gmail.com>
2020-05-03 10:48:30 +0300

    Add email column to users table; configure User class to work with flask-login by importing UserMixin and passing it as a parameter to the User class; create a new migration to reflect the changes precipitated by the addition of new columns

--- app/models.py
+++ app/models.py
@@ -1,16 +1,27 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+
+    '''
+    function retrieves a user with a given id
+    '''
+    return User.query.get(int(user_id))
+
+class User(db.Model,UserMixin):
 
     '''
     class faciliates the creation of user objects
     '''
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique=True,index=True)
+    username = db.Column(db.String(255),index=True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
-    password_secure = db.Column(db.String(255))
+    password_hash = db.Column(db.String(255))
     
     @property
     def password(self):
